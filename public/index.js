a51a7a491c3617d53fb59da570b284d8a2abe139
Violet Kim <violetshirokuma@gmail.com>
2020-05-13 17:52:07 -0700

    Adding all files completed

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ fetch("/api/transaction")
     return response.json();
   })
   .then(data => {
-    // save db data on global variable
+
     transactions = data;
 
     populateTotal();
@@ -15,7 +15,7 @@ fetch("/api/transaction")
   });
 
 function populateTotal() {
-  // reduce transaction amounts to a single total value
+
   let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
   }, 0);
@@ -29,7 +29,7 @@ function populateTable() {
   tbody.innerHTML = "";
 
   transactions.forEach(transaction => {
-    // create and populate a table row
+
     let tr = document.createElement("tr");
     tr.innerHTML = `
       <td>${transaction.name}</td>
@@ -45,19 +45,19 @@ function populateChart() {
   let reversed = transactions.slice().reverse();
   let sum = 0;
 
-  // create date labels for chart
+  // create date labels
   let labels = reversed.map(t => {
     let date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   });
 
-  // create incremental values for chart
+  // create incremental values
   let data = reversed.map(t => {
     sum += parseInt(t.value);
     return sum;
   });
 
-  // remove old chart if it exists
+  // remove old chart if exists
   if (myChart) {
     myChart.destroy();
   }
